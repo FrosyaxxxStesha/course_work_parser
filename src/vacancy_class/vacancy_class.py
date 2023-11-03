ecdd77b0461c30f7cc8065ccbfb826601c9fab96
Tsurkan <mikh.tsurkan@gmail.com>
2023-11-03 03:14:46 +0300

    add validation

--- src/vacancy_class/vacancy_class.py
+++ src/vacancy_class/vacancy_class.py
@@ -1,3 +1,15 @@
 class Vacancy:
-    def __init__(self, *args):
-        pass
+    def __init__(self, title, url, salary, requirements, responsibility):
+        if not isinstance(str, title):
+            raise ValueError("Название должно быть строкой")
+        if not isinstance(str, url):
+            raise ValueError("ссылка должна быть строкой")
+        if not isinstance(int, salary):
+            raise ValueError("Зарплата должна быть целым числом")
+        if not (isinstance(str, requirements) or requirements is None):
+            raise ValueError("Требования должны быть строкой")
+        if not (isinstance(str, responsibility) or responsibility is None):
+            raise ValueError("Обязанности должны быть строкой")
+
+
+
