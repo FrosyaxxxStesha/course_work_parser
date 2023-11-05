edb44c12a3ecee00dc0f06700550a45f7a3b28ed
Tsurkan <mikh.tsurkan@gmail.com>
2023-11-05 17:51:25 +0300

    add jsonsaving

--- src/savers/specialized_savers/json_saving.py
+++ src/savers/specialized_savers/json_saving.py
@@ -1,1 +1,20 @@
 from src.savers import AbstractSaving
+from src.vacancy_classes.vacancy_operator import VacancyOperator
+from json import dumps, loads
+
+
+class JSONSaving(AbstractSaving):
+    def _parse_operator(self, file_name) -> VacancyOperator:
+        with open(file_name, "r") as fp:
+            json_string = fp.read()
+
+        operator_dict = loads(json_string)
+        return VacancyOperator.from_dict(operator_dict)
+
+    def _operator_to_file(self, file_name) -> None:
+        json_string = dumps(self.__operator, indent=2, ensure_ascii=False)
+
+        with open(file_name, "w") as fp:
+            fp.write(json_string)
+
+
